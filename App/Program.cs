a9e4e79916378fbeb9e21ad6cf46ffa5b6f5759a
iceStorm <anhtuan3675@outlook.com>
2020-11-09 02:27:13 +0700

    re-structured project

--- App/Program.cs
+++ App/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace ShoppingApp
+namespace App
 {
     public class Program
     {
