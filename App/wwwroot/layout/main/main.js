9b41839a8fc5ce316ac6433fca115ba5df6e028c
iceStorm <anhtuan3675@outlook.com>
2020-12-20 12:07:42 +0700

    add scrollbar padding to the header

--- App/wwwroot/layout/main/main.js
+++ App/wwwroot/layout/main/main.js
@@ -2,4 +2,41 @@
 $(document).ready(() => {
 
     $('#content').css('padding-top', $('#header').innerHeight() + 30);
+
+
+    if ($('#content').hasScrollBar()) {
+        let scrollbarWidth = getScrollbarWidth();
+        let headerPadding = $('#header').css('padding-right');
+        console.log(scrollbarWidth, headerPadding);
+        $('#header').css('padding-right', parseInt(headerPadding) + scrollbarWidth);
+    }
 });
+
+
+function getScrollbarWidth() {
+
+    // Creating invisible container
+    const outer = document.createElement('div');
+    outer.style.visibility = 'hidden';
+    outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+    outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+    document.body.appendChild(outer);
+
+    // Creating inner element and placing it in the container
+    const inner = document.createElement('div');
+    outer.appendChild(inner);
+
+    // Calculating difference between container's full width and the child width
+    const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+    // Removing temporary elements from the DOM
+    outer.parentNode.removeChild(outer);
+
+    return scrollbarWidth;
+}
+
+(function ($) {
+    $.fn.hasScrollBar = function () {
+        return this[0] ? this[0].scrollHeight > this.innerHeight() : false;
+    }
+})(jQuery);
